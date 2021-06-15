3b3117a5d61ed1c0a36b8f0c4276e4a287900260
therealcoolpup <azcooldude8@gmail.com>
2021-06-16 01:26:27 +1000

    Started movement

---
+++ Block Buster/Assets/Scripts/Universal/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject _topPlayer;
+    public GameObject _bottomPlayer;
+    public float _speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+            _topPlayer.transform.position = transform.forward / _speed;
+        }
+    }
+}
