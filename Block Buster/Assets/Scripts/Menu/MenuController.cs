2b9f58cc79bb14209f29e6f6efd7041e6644508c
therealcoolpup <azcooldude8@gmail.com>
2021-06-16 00:34:11 +1000

    Setup menu

---
+++ Block Buster/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void Play()
+    {
+        print("Loaded first scene.");
+        SceneManager.LoadScene("1");
+    }
+
+    public void Options()
+    {
+       
+        print("Opened settings.");
+    }
+
+    public void Exit()
+    {
+        print("Quit game.");
+        Application.Quit();
+    }
+}
