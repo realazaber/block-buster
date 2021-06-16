489a1c2221e8fb822c65051a65dac119c7cd1697
therealcoolpup <azcooldude8@gmail.com>
2021-06-17 04:35:13 +1000

    Top player can move but need to fix jump

--- Block Buster/Assets/Scripts/Universal/Player/PlayerController.cs
+++ Block Buster/Assets/Scripts/Universal/Player/PlayerController.cs
@@ -6,17 +6,55 @@ public class PlayerController : MonoBehaviour
 {
 
     public GameObject _topPlayer;
+    public Rigidbody _topPlayerRB;
+
     public GameObject _bottomPlayer;
+    public Rigidbody _bottomPlayerRB;
+
     public float _speed;
+    public float _jumpHeight;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _speed *= 2;
+            print("Sprint speed " + _speed);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _speed /= 2;
+            print("Walk speed " + _speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
         {
             
-            _topPlayer.transform.position = transform.forward / _speed;
+            _topPlayer.transform.position += transform.right * _speed * Time.deltaTime;
+            print("Top player move right");
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+
+                _topPlayerRB.AddForce(transform.up * _jumpHeight);
+                print("Top player jump");
+            }
         }
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+            _topPlayer.transform.position -= transform.right * _speed * Time.deltaTime;
+            print("Top player move left");
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+
+                _topPlayerRB.AddForce(transform.up * _jumpHeight);
+                print("Top player jump");
+            }
+        }
+  
+
     }
 }
