09a699563a45088aa997f9cf3e392350d5f15068
therealcoolpup <azcooldude8@gmail.com>
2021-06-17 05:50:13 +1000

    Pause menu working

--- Block Buster/Assets/Scripts/Leve Specific/Menu/MenuController.cs
+++ Block Buster/Assets/Scripts/Leve Specific/Menu/MenuController.cs
@@ -5,16 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
+    public string _scene;
+
+    public GameObject _world;
+    public GameObject _menu;
+
+    public bool _isLevel = true;
+
+    private void Update()
+    {
+        if (_isLevel && Input.GetKey(KeyCode.Escape))
+        {
+            _world.SetActive(false);
+            _menu.SetActive(true);
+        }
+        
+        if (_isLevel && Input.GetKey(KeyCode.P))
+        {
+            _world.SetActive(true);
+            _menu.SetActive(false);
+        }
+    }
+
     public void Play()
     {
-        print("Loaded first scene.");
-        SceneManager.LoadScene("1");
+        print("Loaded scene " + _scene);
+        SceneManager.LoadScene(_scene);
+    }
+
+    public void Resume()
+    {
+        _world.SetActive(true);
+        _menu.SetActive(false);
     }
 
     public void Options()
     {
        
         print("Opened settings.");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void Exit()
